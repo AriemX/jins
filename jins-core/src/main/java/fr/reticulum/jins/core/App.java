f5e45d0c6af35c219214e506773ebd4e94df1bac
guillaume Claude <guillaume.jf.claude@live.fr>
2012-07-14 20:31:12 +0200

    Architecture du projet

---
+++ jins-core/src/main/java/fr/reticulum/jins/core/App.java
@@ -0,0 +1,13 @@
+package fr.reticulum.jins.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
